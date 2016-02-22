16878addc49750214afa3325ddc2bf8381b947ab
Dean <dean@traumasoft.com>
2016-02-21 22:21:04 -0500

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -19,18 +20,28 @@ app.get('/todos', function(req, res) {
 
 // GET /todos/[ID]
 app.get('/todos/:id', function(req, res) {
-	for(var i = 0; i < todos.length; i++) {
-		if(todos[i].id === Number(req.params.id)) {
-			return res.json(todos[i]);
-		}
-	}
+	var todo = _.findWhere(todos, {id: parseInt(req.params.id, 10)});
+	
+	if(todo) {
+		return res.json(todo);
+    }
 
 	res.status(404).send();
 });
 
 app.post('/todos', function(req, res) {
-	var todo = req.body;
+	var todo = _.pick(req.body,'description','completed');
+
+	if(!_.isString(todo.description) || todo.description.trim().length === 0) {
+		return res.status(400).send("Property 'description' required and must be string");
+	}
+
+	if(!_.isBoolean(todo.completed)) {
+		return res.status(400).send("Property 'completed' must be boolean");
+	}
+
 	todo.id = todoNextId++;
+	todo.description = todo.description.trim();
 	todos.push(todo);
 
 	console.log('todos',JSON.stringify(todos,null,4));
