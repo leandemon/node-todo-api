1e22c306010c174add640546f47ddd95b335eb25
Dean <dean@traumasoft.com>
2016-03-02 01:09:24 -0500

    Add user model and POST /users request

---
+++ models/user.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('user', {
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: true,
+				len: [1, 255],
+				isString: function (value) {
+					if (typeof value !== 'string') {
+						throw new Error('Email address must be a string');
+					}
+				}
+			}
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [7, 100],
+				isString: function (value) {
+					if (typeof value !== 'string') {
+						throw new Error('Password must be a string');
+					}
+				}
+			}
+		}
+	});
+}
